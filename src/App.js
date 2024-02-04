cb1d6dd17c23c658522eda76aeab10549de6871b
AnkushUjawane <multiverse1808@gmail.com>
2024-02-05 01:45:50 +0530

    mentor page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Mentor from "./Components/MentorsPage/mentor.js";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <>
+      <div className="App">
+        <Mentor />
+        {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,8 +19,9 @@ function App() {
         >
           Learn React
         </a>
-      </header>
-    </div>
+      </header> */}
+      </div>
+    </>
   );
 }
 
