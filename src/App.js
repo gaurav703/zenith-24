3a649227d09dcd43779397977631b0e0a84a9a6f
PrashikBhimte <2022bcs007@sggs.ac.in>
2024-02-07 03:20:09 +0530

    loader issue sloved

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   useEffect(() => {
     setTimeout(() => {
       setLoading(false);
-    }, 1500);
+    }, 3000);
   }, []);
 
   return (
--- src/Components/homepage/homepage.jsx
+++ src/Components/homepage/homepage.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import Heropage from '../heropage/heropage'
-import About from '../about/about'
-import Navbar from '../navbar/navbar'
+import React from 'react';
+import Heropage from '../heropage/heropage';
+import About from '../about/about';
+import Navbar from "../navbar/navbar.jsx";
 
 export default function Homepage() {
   return (
     <div>
-        <Navbar />
+    <Navbar />
         <Heropage />
         <About />
     </div>
