1c991fdb94a482f98d477c857da084efc824c5b1
gaurav703 <111629507+gaurav703@users.noreply.github.com>
2024-02-09 03:09:26 +0530

    event page update

--- src/Components/Event/Event.jsx
+++ src/Components/Event/Event.jsx
@@ -1,71 +1,97 @@
 import React, { useState, useEffect } from "react";
 import "./Event.css";
 import Model from "./Model";
+import pdf from "./Zenith.pdf";
 
 const sportsData = [
   {
     image:
       "https://res.cloudinary.com/dqki29mbg/image/upload/v1707415831/Event-Page/dhz3zwc6ebpiytudgmpr.png",
     name: "Cricket",
-    boys: "Boys : 1200₹",
-    girls: "Girls : 1200₹",
-    contact: "Vivek: 9359502953 , Gaurav : 9757398688",
+    boys: "Boys : 6000₹",
+    // girls: "Girls : 1200₹",
+    contact1: "Om Dhande: - 74990 65743 ",
+    contact2: "Kuntal Lawate: 70583 75155",
   },
   {
     image:
       "https://res.cloudinary.com/dqki29mbg/image/upload/v1707415833/Event-Page/xcul6wzqejyuxxh4bjgu.png",
     name: "Football",
-    boys: "Boys : 1200₹",
-    girls: "Girls : 1200₹",
-    contact: "Vivek: 9359502953 , Gaurav : 9757398688",
+    boys: "Boys : 3000₹",
+    // girls: "Girls : 1200₹",
+    // contact: "Sainath Shettiwar: 9604875754 , Devanshu kothe : 9130707352",
+    contact1: "Sainath Shettiwar: 9604875754",
+    contact2: "Devanshu kothe : 9130707352",
   },
   {
     image:
       "https://res.cloudinary.com/dqki29mbg/image/upload/v1707415833/Event-Page/xcul6wzqejyuxxh4bjgu.png",
     name: "RingFootball",
-    boys: "Boys : 1200₹",
-    girls: "Girls : 1200₹",
-    contact: "Vivek: 9359502953 , Gaurav : 9757398688",
+    boys: "Boys : 2000₹",
+    // girls: "Girls : 1200₹",
+    // contact: "Abhishek avhad: 8329896467 , Fazlur Rahman : 8767221998",
+    contact1: "Abhishek avhad: 8329896467",
+    contact2: "Fazlur Rahman : 8767221998",
   },
   {
     image:
       "https://res.cloudinary.com/dqki29mbg/image/upload/v1707415829/Event-Page/jcqmiwan90zxdjqepfax.png",
     name: "Chess",
-    boys: " Quad : 1200₹",
-    girls: "Solo : 1200₹",
-    contact: "Vivek: 9359502953 , Gaurav : 9757398688",
+    boys: "Team : 1500₹",
+    girls: "Solo : 400₹",
+    // contact: "Vaishnavi paul : 90114 69945 , Aditya chincholkar : 7385041646",
+    contact1: "Vaishnavi paul : 90114 69945",
+    contact2: "Aditya chincholkar : 7385041646",
   },
   {
     image:
       "https://res.cloudinary.com/dqki29mbg/image/upload/v1707415835/Event-Page/fhvd0hjerpv8pmlbkhse.png",
     name: "Volleyball",
-    boys: "Boys : 1200₹",
-    girls: "Girls : 1200₹",
-    contact: "Vivek: 9359502953 , Gaurav : 9757398688",
+    boys: "Boys : 2200₹",
+    girls: "Girls : 1800₹",
+    //contact: "Ayush Gahukar: 7666693350 , Pranay Rathod: 8767410458",
+    contact1: "Ayush Gahukar: 7666693350",
+    contact2: "Pranay Rathod: 8767410458",
   },
   {
     image:
       "https://res.cloudinary.com/dqki29mbg/image/upload/v1707415829/Event-Page/wapi2dnoivdrd4e3cofq.png",
     name: "Kabaddi",
+    boys: "Boys : 1700₹",
+    girls: "Girls : 1500₹",
+    //  contact: "Sushant Pradhan: 8836025310 , Nikul Jadhao : 8836025310",
+    contact1: "Sushant Pradhan: 8836025310",
+    contact2: "Nikul Jadhao : 8836025310",
+  },
+  {
+    image:
+      "https://res.cloudinary.com/dqki29mbg/image/upload/v1707415829/Event-Page/wapi2dnoivdrd4e3cofq.png",
+    name: "Kho-Kho",
     boys: "Boys : 1200₹",
-    girls: "Girls : 1200₹",
-    contact: "Vivek: 9359502953 , Gaurav : 9757398688",
+    // girls: "Girls : 1200₹",
+    //  contact: "Sushant Pradhan: 8836025310 , Nikul Jadhao : 8836025310",
+    contact1: "Gaurav Nayase: 7249849725",
+    // contact2: "Nikul Jadhao : 8836025310",
   },
   {
     image:
       "https://res.cloudinary.com/dqki29mbg/image/upload/v1707415834/Event-Page/dsxvtc0pgyta5mpyprkt.png",
     name: "Handball",
-    boys: "Boys : 1200₹",
+    boys: "Boys : 1500₹",
     girls: "Girls : 1200₹",
-    contact: "Vivek: 9359502953 , Gaurav : 9757398688",
+    //contact: "Aniket Dharwar: 96733 52236 , Udedhan Sonawane : 8888964392",
+    contact1: "Aniket Dharwar: 96733 52236",
+    contact2: "Udedhan Sonawane : 8888964392",
   },
   {
     image:
       "https://res.cloudinary.com/dqki29mbg/image/upload/v1707415833/Event-Page/apa36dxqwxmevwkgl5hl.png",
     name: "Basketball",
-    boys: "Boys : 1200₹",
-    girls: "Girls : 1200₹",
-    contact: "Vivek: 9359502953 , Gaurav : 9757398688",
+    boys: "Boys : 2500₹",
+    girls: "Girls : 2000₹",
+    // contact: "Dishant Sawwalakhe: 9699120082 , Shakib : 9960844980",
+    contact1: "Dishant Sawwalakhe: 9699120082",
+    contact2: "Shakib : 9960844980",
   },
 ];
 
@@ -77,6 +103,16 @@ const Event = () => {
     setSelectedSport(sport);
     setPop(true);
   };
+
+  function download(pdfpath) {
+    const pdfUrl = pdfpath;
+    const link = document.createElement("a");
+    link.href = pdfUrl;
+    link.download = "download";
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  }
 
   return (
     <div className="main-container">
@@ -93,7 +129,7 @@ const Event = () => {
             <h2>{sport.name}</h2>
             <div className="info">
               <h2>{sport.name}</h2>
-              <p className="fees">Entry Fee: </p>
+              <p className="fees">Entry Fee </p>
               <div className="price">
                 <p>
                   {sport.boys}
@@ -109,7 +145,7 @@ const Event = () => {
                   id="downloadBtn"
                   value="download"
                   className="download"
-                  onClick={() => console.log("Download logic goes here")}
+                  onClick={() => download(pdf)}
                 >
                   <img
                     src="https://res.cloudinary.com/dqki29mbg/image/upload/v1707415831/Event-Page/bsomh2xhrbiuz6jpe9ah.png"
